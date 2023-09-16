264e07f6f9a756a90d9d9caa34cfcae29af3eec6
quintenrosseel <quintenrosseel>
2023-09-16 21:14:29 +0000

    Added new visualisations

--- start_here.py
+++ start_here.py
@@ -42,19 +42,15 @@ import ydata_profiling
 # COMMAND ----------
 
 # MAGIC %md 
-# MAGIC **Adding your dataset**
+# MAGIC ## Reading in the dataset
+# MAGIC
 # MAGIC - Execute the command below to get information such as `YOUR_EMAIL`, `YOUR_REPO_NAME`. 
 # MAGIC - Add your dataset in the `data` directory using the `import` button in the UI. 
+# MAGIC - Make sure to fill in `YOUR_EMAIL` & `YOUR_REPO_NAME` to ensure that Databricks reads in the file correctly. 
 
 # COMMAND ----------
 
 dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().get()
-
-# COMMAND ----------
-
-# MAGIC %md 
-# MAGIC ## Reading in the dataset
-# MAGIC Make sure to fill in `YOUR_EMAIL` & `YOUR_REPO_NAME` to ensure that Databricks reads in the file correctly. 
 
 # COMMAND ----------
 
@@ -160,6 +156,169 @@ histogram_vis_df = (
     .pipe(pd.DataFrame.rename, columns={c: c + '_CNT' for c in accidents_2021_df.columns})
 )   
 display(histogram_vis_df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC
+# MAGIC # Common Operations
+# MAGIC
+# MAGIC - `Grouping`: calculate statistics over certain groups of variables 
+# MAGIC - `Merging`: joining two dataframes together
+# MAGIC - `Masking`: filtering a DataFrame
+# MAGIC - `Imputing`: filling in missing data with the `mean`, `mode`, `median`. 
+# MAGIC - `Scaling`: Mapping your range in another range (ML)
+
+# COMMAND ----------
+
+accidents_2021_df.head()
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC
+# MAGIC ## Grouping
+
+# COMMAND ----------
+
+group_cols = [
+    "CD_REFNIS"
+]
+
+stat_cols = [
+    "CD_SEX",
+    "CD_NATLTY",
+    "TX_NATLTY_NL",
+    "CD_CIV_STS",
+    "TX_CIV_STS_NL",
+    "CD_AGE",
+    "MS_POPULATION"
+]
+
+def flatten_mindex(df: pd.DataFrame) -> pd.DataFrame: 
+    """
+        Flatten a multi-index as a result of a groupby-describe operation. 
+    """
+    df.columns = [l1 + '_' + l2 for l1, l2 in df.columns]
+    return df
+
+def recreate_index(
+        df: pd.DataFrame) -> pd.DataFrame: 
+    """ 
+        Reset the index and return a random, integer-based index.      
+    """
+    df.reset_index(inplace=True)
+    return df 
+
+muncipop_distribution_df = (
+    population_2021_df.copy(deep=True)
+    .pipe(lambda df: df[group_cols + stat_cols])
+    .groupby(by=group_cols, as_index=True)
+    .describe()
+    .pipe(flatten_mindex)
+    .pipe(recreate_index)
+)
+
+muncipop_distribution_df.head()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC ## Merging
+# MAGIC Best way to think of this, is to imagine two venn diagrams. 
+# MAGIC - Left/right join: keep all entries of left/right dataframe resp., add NaN when no match is found in the right
+# MAGIC - Inner join: keep only entries where join key is in both 
+# MAGIC - Outer: keep all entries
+
+# COMMAND ----------
+
+print("There are", len(accidents_2021_df), "accidents")
+print("There are", len(muncipop_distribution_df) , "population measurements")
+print("There are", len(accidents_2021_df.CD_MUNTY_REFNIS.unique()), "muncipalities in the accidents dataset")
+print("There are", len(muncipop_distribution_df.CD_REFNIS.unique()), "in the muncipality dataset")
+
+# COMMAND ----------
+
+# If you'd play with left or right joins, the filter will return a rows or not. 
+muncip_accidents_df = (
+    accidents_2021_df.merge(
+        right=muncipop_distribution_df, 
+        how='right', 
+        right_on="CD_REFNIS",
+        left_on="CD_MUNTY_REFNIS"
+    )
+)
+
+# Example: cities where no accident was registered 
+muncip_accidents_df[
+    muncip_accidents_df.DT_DAY.isna()
+]
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC
+# MAGIC ## Masking
+# MAGIC Filtering dataframes to include/exclude certain information.
+
+# COMMAND ----------
+
+(muncip_accidents_df.CD_COLL_TYPE == 5.0).sum()
+
+# COMMAND ----------
+
+# Boolean mask 
+pedestrian_accident_mask = (
+    # Met een voetganger
+    (muncip_accidents_df.CD_COLL_TYPE == 5.0) 
+) 
+
+group_cols = [
+    'CD_REFNIS',
+    'CD_AGE_count',
+    'CD_AGE_mean',
+    'CD_AGE_std',
+    'CD_AGE_min',
+    'CD_AGE_25%',
+    'CD_AGE_50%',
+    'CD_AGE_75%',
+    'CD_AGE_max'
+]
+
+def discretize_column(
+        df: pd.DataFrame, 
+        target_col: str, 
+        source_col: str) -> pd.DataFrame: 
+    df[target_col] = df[source_col].astype(np.int64)
+    return df
+
+
+# Average age of muncip with pedestrian accidents 
+pedestrian_accidents_stats_df = (
+    muncip_accidents_df[pedestrian_accident_mask]
+    .pipe(lambda df: df[select_cols])
+    .groupby(by=select_cols, as_index=False)
+    .agg(
+        CD_REFNIS_COUNT=('CD_REFNIS', 'count')
+    )
+    .pipe(discretize_column, target_col='CD_AGE_mean_disc', source_col='CD_AGE_mean')
+)
+
+pedestrian_accidents_stats_df.display()
+
+# COMMAND ----------
+
+len(pedestrian_accidents_stats_df.CD_AGE_mean_disc.unique())
+
+# COMMAND ----------
+
+# The ~ is a negation operation, so we take all 
+accident_mask = muncip_accidents_df[
+    ~muncip_accidents_df.DT_DAY.isna()
+]
+
 
 # COMMAND ----------
 
