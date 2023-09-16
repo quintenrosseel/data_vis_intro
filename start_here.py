6d1fadb2850f055b2234052a0e46f2151a767fd8
quintenrosseel <quintenrosseel>
2023-09-16 17:19:28 +0000

    Added dataset

--- start_here.py
+++ start_here.py
@@ -121,3 +121,71 @@ eda_report = ProfileReport(accidents_2021_df)
 
 # Show the report
 eda_report
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC
+# MAGIC ## Refining your data story 
+# MAGIC With the dataset below, we have a lot of information that can help us to create a juicy story. 
+# MAGIC From here, you can drill down using libraries such as 
+# MAGIC
+# MAGIC - `matplotlib`: the go-to python library for data visualisation. 
+# MAGIC - `seaborn`: an extension of matplotlib to create more appealing visualisations. 
+# MAGIC - `Plotly`: the go-to python library for interactive plots. Note: Databricks has an inline integration of Plotly. 
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC
+# MAGIC ## Example 
+# MAGIC Refining your dataset for more comprehensive visualisations. 
+
+# COMMAND ----------
+
+# Example data transformations 
+
+# Create a map for day of the week. 
+days_of_week = {
+    1: 'monday',
+    2: 'tuesday',
+    3: 'wednesday',
+    4: 'thursday',
+    5: 'friday',
+    6: 'saturday',
+    7: 'sunday'
+}
+
+def add_weekday_en(
+        df: pd.DataFrame, 
+        weekday_target_col: str = 'weekday', 
+        weekday_source_col: str = 'CD_DAY_OF_WEEK', 
+        map_dict = None) -> pd.DataFrame: 
+    """
+        Adds an english day of the week, using a dictionary map. 
+    """
+    df[weekday_target_col] = df[weekday_source_col].map(map_dict)
+    return df 
+
+# Create the visualisation dataset 
+histogram_vis_df = (
+    accidents_2021_df.copy(deep=True)
+    # Add an english day of the week
+    .pipe(add_weekday_en, map_dict=days_of_week)
+    # Sort by day of the week
+    .pipe(pd.DataFrame.sort_values, by=['CD_DAY_OF_WEEK'], ascending=True)
+    # Groupby & Count number of accidents per day of the week. 
+    .groupby(['weekday'], as_index=False, sort=False).count()
+    # Rename columns to account for COUNT operation
+    .pipe(pd.DataFrame.rename, columns={c: c + '_CNT' for c in accidents_2021_df.columns})
+)   
+display(histogram_vis_df)
+
+# COMMAND ----------
+
+histogram_vis_df
+
+# COMMAND ----------
+
+[# Now it's up to you. 
+# Check your data to gain a better understanding and insight that you want to 
